1282dc235ac2dc5ec6dac9af6fcff3b8e6cb2b0b
asfiaaiman <aiman.asfia@gmail.com>
2024-07-27 18:38:19 +0500

    Login, reset password, and forgot password pages are ready

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "@/views/Dashboard.vue";
 import Login from "@/views/Login.vue";
+import ForgotPassword from "@/views/ForgotPassword.vue";
+import ResetPassword from "@/views/ResetPassword.vue";
 
 const routes = [
     {
@@ -12,6 +14,16 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
+    },
+    {
+        path: '/reset-password',
+        name: 'reset-password',
+        component: ResetPassword
+    },
+    {
+        path: '/forgot-password',
+        name: 'forgot-password',
+        component: ForgotPassword
     }
 ];
 
