6c8e63828d1c5bde33e04c48b404420eb580bec1
asfiaaiman <aiman.asfia@gmail.com>
2024-08-01 18:35:19 +0500

    Layout for an-auth users has been created

---
+++ backend/src/components/GuestLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+
+        <div class="w-full max-w-md space-y-8">
+            <div>
+                <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">
+                    {{ title }}
+                </h2>
+            </div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<!-- Composition API -->
+<script setup>
+
+const {title} = defineProps({
+    title: 'String'
+})
+
+</script>
+
+<style scoped></style>
--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,41 +1,44 @@
 <template>
-    <div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+    <!-- Guest Layout -->
+    <GuestLayout title="Sign in to your Account">
+        <input type="hidden" name="remember" value="true">
 
-      <div class="w-full max-w-md space-y-8">
         <div>
-            <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900">
-                Sign into Your Account
-            </h2>
-        </div>
-        <form class="space-y-6" action="#" method="POST">
-          <div>
             <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div class="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input id="email" name="email" type="email" autocomplete="email" required=""
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
-          </div>
+        </div>
 
-          <div>
+        <div>
             <div class="flex items-center justify-between">
-              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-              <div class="text-sm">
-                <a href="/forgot-password" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-              </div>
+                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                <div class="text-sm">
+                    <a href="/forgot-password" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
+                        password?</a>
+                </div>
             </div>
             <div class="mt-2">
-              <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input id="password" name="password" type="password" autocomplete="current-password" required=""
+                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
-          </div>
+        </div>
 
-          <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </template>
+        <div>
+            <button type="submit"
+                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                in</button>
+        </div>
+
+    </GuestLayout>
+</template>
+
+<script setup>
+import { LockClosedIcon } from '@heroicons/vue/24/solid'
+import GuestLayout from '../components/GuestLayout.vue';
+
+</script>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
